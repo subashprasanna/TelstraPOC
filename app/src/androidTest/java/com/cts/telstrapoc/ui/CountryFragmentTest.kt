809a56ff600c859676e2b29d94615477eca0c4a4
BH15123 <prasanna.b@macys.com>
2020-04-04 19:17:02 +0530

    Completed Mockk test

---
+++ app/src/androidTest/java/com/cts/telstrapoc/ui/CountryFragmentTest.kt
@@ -0,0 +1,19 @@
+package com.cts.telstrapoc.ui
+
+import android.R
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class CountryFragmentTest {
+
+   @Test
+   fun test() {
+
+   }
+}
